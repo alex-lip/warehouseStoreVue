ba62351c3ef1fbbffc12601b2d221528af940611
Alex <alex.lipinsky@gmail.com>
2021-03-24 16:02:02 -0400

    Home categories component created

--- src/components/HomeHero.vue
+++ src/components/HomeHero.vue
@@ -2,7 +2,7 @@
   <div class="container-fluid">
       <div class="row d-flex justify-content-center my-2 mx-lg-3 mx-sm-2">
           <div class="col-lg-7 hero mr-lg-2 mb-2 d-flex flex-column justify-content-start">
-            <h2 class="pt-5 lg text-center">The easiest way to find the right bearing.</h2>
+            <h2 class="pt-3 lg text-center">The easiest way to find the right bearing.</h2>
             <p class="pt-3 md text-center">Our goal is to build the most comprehensive and best organized product
                 catalog of bearing and mechanical power transmission products.
                 </p>
@@ -28,13 +28,6 @@ export default {
 </script>
 
 <style>
-/*
-.hero {
-    height: 300px;
-    background-image: url('https://images.unsplash.com/photo-1553413077-190dd305871c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixpb=rb-1.2.1&auto=format&fit=crop&w=750&q=80');
-    background-color: var(--primary-color);
-    background-position-y: 60%;
-} */
 
 .hero {
     background-image: linear-gradient(
@@ -50,7 +43,7 @@ export default {
     hsla(0, 0%, 0%, 0.9) 100%
   ),
     url('https://images.unsplash.com/photo-1553413077-190dd305871c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixpb=rb-1.2.1&auto=format&fit=crop&w=750&q=80');
-    height: 300px;
+    height: 260px;
     background-size: cover;
     background-position-y: 60%;
     color: white;
@@ -58,7 +51,7 @@ export default {
 
 
 .hero-2 {
-    height: 300px;
+    height: 260px;
     background-color: var(--primary-color);
     color: #FFF;
 }
@@ -67,7 +60,7 @@ export default {
 .hero-2 p {
     font-size: 16px;
     font-weight: 300;
-    padding-top: 10px;
+    padding-top: 5px;
 }
 
 </style>
