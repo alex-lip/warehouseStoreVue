7e485d0b77de06b898cc5806b54c26d377929a7c
Alex <alex.lipinsky@gmail.com>
2021-03-24 21:07:20 -0400

    product category section

--- src/components/HomeCategories.vue
+++ src/components/HomeCategories.vue
@@ -1,28 +1,139 @@
 <template>
-  <div class="container-sm">
-      <div class="row">
-          <div class="col-lg-2 col-md-3 col-sm-4 col-5">Bearings<i class="fas fa-angle-right pl-1"></i></div>
-          <div class="col-lg-2 col-md-3 col-sm-4 col-5">Bearings</div>
-          <div class="col-lg-2 col-md-3 col-sm-4 col-5">Bearings</div>
-          <div class="col-lg-2 col-md-3 col-sm-4 col-5">Bearings</div>
-          <div class="col-lg-2 col-md-3 col-sm-4 col-5">Bearings</div>
-          <div class="col-lg-2 col-md-3 col-sm-4 col-5">Bearings</div>
-          <div class="col-lg-2 col-md-3 col-sm-4 col-5">Bearings</div>
-          <div class="col-lg-2 col-md-3 col-sm-4 col-5">Bearings</div>
-          <div class="col-lg-2 col-md-3 col-sm-4 col-5">Bearings</div>
-          <div class="col-lg-2 col-md-3 col-sm-4 col-5">Bearings</div>
-          <div class="col-lg-2 col-md-3 col-sm-4 col-5">Bearings</div>
-          <div class="col-lg-2 col-md-3 col-sm-4 col-5">Bearings</div>
+  <div class="container-fluid">
+    <div class="row product-group">
+      <div class="col-md-5">
+        <h2 class="m-2 ml-4">Bearings</h2>
+        <img
+          src="https://1m28fbq9etc1rj5ga1fu0k61-wpengine.netdna-ssl.com/wp-content/uploads/2016/11/TS-etched.png"
+          alt="Tapered Roller Bearings"
+          width="400px"
+          class="ml-5"
+        />
       </div>
+      <div class="col-md-7">
+        <div class="row">
+          <div
+            class="product-box d-flex justify-content-start align-items-center col-md-3 col-sm-5 col-5"
+          >
+            <img
+                src="https://pixy.org/src/479/thumbs350/4794045.jpg"
+              
+              alt="Ball Bearing"
+              height="100px"
+            />
+            <p>Ball Bearings</p>
+          </div>
+          <div
+            class="product-box d-flex justify-content-start align-items-center col-md-3 col-sm-5 col-5"
+          >
+            <img
+              src="https://1m28fbq9etc1rj5ga1fu0k61-wpengine.netdna-ssl.com/wp-content/uploads/2016/11/TS-etched.png"
+              alt="Roller Bearings"
+              width="120px"
+            />
+            <p>Roller Bearings</p>
+          </div>
+          <div
+            class="product-box d-flex justify-content-start align-items-center col-md-3 col-sm-5 col-5"
+          >
+            <img
+              src="https://1m28fbq9etc1rj5ga1fu0k61-wpengine.netdna-ssl.com/wp-content/uploads/2016/11/TS-etched.png"
+              alt="Mounted Bearings"
+              width="120px"
+            />
+            <p>Mounted Bearings</p>
+          </div>
+          <div
+            class="product-box d-flex justify-content-start align-items-center col-md-3 col-sm-5 col-5"
+          >
+            <img
+              src="https://1m28fbq9etc1rj5ga1fu0k61-wpengine.netdna-ssl.com/wp-content/uploads/2016/11/TS-etched.png"
+              alt="Tapered Roller Bearings"
+              width="120px"
+            />
+            <p>Thrust Bearings</p>
+          </div>
+          <div
+            class="product-box d-flex justify-content-start align-items-center col-md-3 col-sm-5 col-5"
+          >
+            <img
+              src="https://1m28fbq9etc1rj5ga1fu0k61-wpengine.netdna-ssl.com/wp-content/uploads/2016/11/TS-etched.png"
+              alt="Tapered Roller Bearings"
+              width="100px"
+            />
+            <p>Spherical Plain Bearings</p>
+          </div>
+          <div
+            class="product-box d-flex justify-content-start align-items-center col-md-3 col-sm-5 col-5"
+          >
+            <img
+              src="https://1m28fbq9etc1rj5ga1fu0k61-wpengine.netdna-ssl.com/wp-content/uploads/2016/11/TS-etched.png"
+              alt="Tapered Roller Bearings"
+              width="120px"
+            />
+            <p>Sleeve Bearings</p>
+          </div>
+          <div
+            class="product-box d-flex justify-content-start align-items-center col-md-3 col-sm-5 col-5"
+          >
+            <img
+              src="https://1m28fbq9etc1rj5ga1fu0k61-wpengine.netdna-ssl.com/wp-content/uploads/2016/11/TS-etched.png"
+              alt="Tapered Roller Bearings"
+              width="120px"
+            />
+            <p>Rod End Bearings</p>
+          </div>
+          <div
+            class="product-box d-flex justify-content-start align-items-center col-md-3 col-sm-5 col-5"
+          >
+            <img
+              src="https://1m28fbq9etc1rj5ga1fu0k61-wpengine.netdna-ssl.com/wp-content/uploads/2016/11/TS-etched.png"
+              alt="Tapered Roller Bearings"
+              width="120px"
+            />
+            <p>Bearing Tools</p>
+          </div>
+          <div
+            class="product-box d-flex justify-content-start align-items-center col-md-3 col-sm-5 col-5"
+          >
+            <img
+              src="https://1m28fbq9etc1rj5ga1fu0k61-wpengine.netdna-ssl.com/wp-content/uploads/2016/11/TS-etched.png"
+              alt="Tapered Roller Bearings"
+              width="120px"
+            />
+            <p>Bearing Accessories</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
+.product-group {
+  background-color: var(--light-color);
+}
 
+.product-box {
+  height: 144px;
+  background-color: #fff;
+  margin: 10px;
+  box-sizing: border-box;
+  flex-direction: column;
+  padding: 16px;
+}
+
+.product-box:hover {
+  box-shadow: 0 0 24px rgba(0, 0, 0, 0.2);
+  transition: box-shadow 0.3s ease-in-out;
+  color: var(--secondary-color);
+}
+
+.product-box p {
+  font-size: 14px;
+}
 </style>
